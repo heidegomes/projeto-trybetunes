c463777a6b8cf2ba7f14ebbc6b218f515bd1e3a6
heidegomes <heidegc@hotmail.com>
2022-10-05 10:11:48 -0300

    Requisito 3 feito!

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,59 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Loading from '../pages/Loading';
+import { getUser } from '../services/userAPI';
 
 class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loginUser: '',
+      isLoading: false,
+    };
+  }
+
+  componentDidMount() {
+    getUser()
+      .then(
+        (result) => {
+          console.log('result: ', result);
+          this.setState({
+            isLoading: true,
+            loginUser: result.name,
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoading: true,
+          });
+          console.log(error);
+        },
+      );
+  }
+
   render() {
-    // const {  } = this.props;
+    const { isLoading, loginUser } = this.state;
     return (
       <header data-testid="header-component">
         <nav>
           <Link to="/search" data-testid="link-to-search">
             Pesquisa
           </Link>
-
+          &nbsp;
           <Link to="/favorites" data-testid="link-to-favorites">
             Favoritos
           </Link>
-
+          &nbsp;
           <Link to="/profile" data-testid="link-to-profile">
             Perfil
           </Link>
+          { !isLoading
+            ? <Loading />
+            : (
+              <div data-testid="header-user-name">
+                { loginUser }
+              </div>
+            )}
         </nav>
       </header>
     );
